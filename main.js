00f0fe421aab068237eda3a9324d0e6c085af5cd
CitlaliMartinez08 <144378895+CitlaliMartinez08@users.noreply.github.com>
2025-11-20 12:17:52 -0600

    n8n

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const N8N_WEBHOOK_URL = 'https://cit8.app.n8n.cloud/webhook-test/e3f93b33-4a35-40d7-818f-624886d921fd';
+const N8N_WEBHOOK_URL = 'https://cit8.app.n8n.cloud/webhook-test/8f8a1415-050b-434b-a5da-e6179d4f7740';
 
 const form = document.getElementById('calculationForm');
 const expressionInput = document.getElementById('mathExpression');
@@ -140,5 +140,6 @@ function displayMessage(message, type) {
     messageDisplay.textContent = message;
     messageDisplay.classList.add(colorClass);
 }
+
 
 
